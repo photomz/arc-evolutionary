66b02320d9f67b2583edaaf565ed9fd21ac9d161
Jeremy Berman <jerber@sas.upenn.edu>
2024-11-06 12:11:03 -0500

    trying to solve multi tests

--- run.py
+++ run.py
@@ -2,38 +2,69 @@ import asyncio
 import json
 from code.data import build_challenges
 from code.logic import solve_challenge
-from code.trees.prod import big_claude_tree, small_claude_tree
+from code.models import GRID
+from code.trees.prod import RootAttemptConfig, big_claude_tree, small_claude_tree
 from pathlib import Path
 
-SOLUTION_TYPE = list[list[list[int]]]
+from pydantic import BaseModel, TypeAdapter
+
+
+class ChallengeSolution(BaseModel):
+    attempt_1: GRID
+    attempt_2: GRID
 
 
 async def run_from_json(
-    *, challenges_path: str, solutions_path: str, limit: int | None
+    *,
+    challenges_path: str,
+    solutions_path: str,
+    tree: list[RootAttemptConfig],
+    limit: int | None,
+    only_run_ids: set[str] = None,
 ) -> None:
     challenges = build_challenges(
         challenges_path=Path(challenges_path), solutions_path=None
     )
+    if only_run_ids:
+        challenges = {k: challenges[k] for k in only_run_ids}
     if limit:
         # only include the first 10 challenges
         challenges = {k: challenges[k] for k in list(challenges)[:limit]}
 
-    solutions_d: dict[str, tuple[SOLUTION_TYPE, SOLUTION_TYPE]] = {}
+    solutions_d: dict[str, list[ChallengeSolution]] = {}
     for challenge_id, challenge in challenges.items():
-        first_solution, second_solution = await solve_challenge(
-            challenge=challenge, tree=small_claude_tree
+        _first_solution, _second_solution = await solve_challenge(
+            challenge=challenge, tree=tree
         )
         # TODO fix, output should be list of solutions
-        solutions_d[challenge_id] = [first_solution], [second_solution]
+        first_solutions: list[GRID] = [_first_solution]
+        second_solutions: list[GRID] = [_second_solution]
+
+        solutions_d[challenge_id] = []
+        for i in range(len(first_solutions)):
+            solutions_d[challenge_id].append(
+                ChallengeSolution(
+                    attempt_1=first_solutions[i],
+                    attempt_2=second_solutions[i],
+                )
+            )
         # just write after each challenge in case program crashes
-        open(solutions_path, "w").write(json.dumps(solutions_d))
+
+        open(solutions_path, "w").write(
+            TypeAdapter(dict[str, list[ChallengeSolution]])
+            .dump_json(solutions_d)
+            .decode("utf-8")
+        )
 
 
 async def run() -> None:
     await run_from_json(
-        challenges_path="test_data/challenges.json",
-        solutions_path="test_data/solutions.json",
-        limit=2,
+        # challenges_path="test_data/challenges.json",
+        challenges_path="test_data/eval_challenges.json",
+        solutions_path="test_data/eval_solutions.json",
+        tree=small_claude_tree,
+        limit=1,
+        only_run_ids={"12997ef3"},
     )
 
 
